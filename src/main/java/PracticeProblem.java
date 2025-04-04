67b16e8edaa61835be409ce454cd890057868514
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-04 13:41:47 -0400

    ???

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,12 +4,12 @@ public class PracticeProblem{
         }
 
         static int towerOfHanoi(int num){
-                /* using a purely recursive function to *solve* the tower of hanoi
-                 * would use bfs instead of dfs, since the tower of hanoi can be 
-                 * represented as a directed unweighted graph, where the shortest
-                 * path must be found, where each state is a node. recursive dfs could
+                /* using a function to *solve* the tower of hanoi would use bfs 
+                 * instead of dfs, since the tower of hanoi can be represented
+                 * as a directed unweighted graph, where the shortest must be
+                 * be found, where each state is a node. recursive dfs could
                  * not solve this problem without iterative deepening, which may disconnect
-                 * itself from the logic of pure recursion. Here is a solution to find the 
+                 * itself from the logic of pure recursion. here is a solution to find the 
                  * number of moves with "recursion" */
                 return num < 3 ? -1 : hanoi(num); 
         }
